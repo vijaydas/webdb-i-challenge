a017a19fe197b78cb96a636bee43bab7598b10eb
Vijay Das <vijaytdas@gmail.com>
2019-06-03 17:37:25 -0500

    add GET

--- server.js
+++ server.js
@@ -1,8 +1,29 @@
 const express = require('express');
+const accountsdb = require("./data/accounts-model.js");
 
 const server = express();
 server.use(express.json());
 
+
 // your code here
+
+
+server.get("/", (req, res) => {
+    accountsdb
+      .find()
+      .then(accounts => {
+        res.status(201).json({
+          accounts
+        });
+      })
+      .catch(err => {
+        res.status(500).json({
+          error: err,
+          message: "no accounts"
+        });
+      });
+  });
+  
+  
 
 module.exports = server;
